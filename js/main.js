30d9d27bae8501af31467246cbcd877ebe193e0f
dev-ashanaur <devashanaur009@gmail.com>
2022-10-17 17:09:14 +0600

    change js code into funtion

--- js/main.js
+++ js/main.js
@@ -1,36 +1,24 @@
 //table 
 const _table = document.getElementById("_table");
-// add row button
-const addRowBtn = document.getElementById("addRow"); 
-// add column button
-const addColumnBtn = document.getElementById("addColumn");
-// add row eventListener
-addRowBtn.addEventListener("click",()=>{
-    // first way
-    // const tblrow = _table.tBodies[0].rows[0].cloneNode(true);
-    // _table.tBodies[0].appendChild(tblrow);
-
-    //second way
+//add row function
+function addRow() {
     const row = _table.insertRow(-1);
-    let i = 0;
-    for(let th of _table.rows[0].children){
+    const cellLength = _table.rows[0].children.length;
+    for (let i = 0; i < cellLength; i++) {
         row.insertCell(i).innerText = "data";
-        i++;
     }
-
-})
-// add column eventListener
-addColumnBtn.addEventListener("click",()=>{
-    // console.log([..._table.rows].entries());
-    for(const [i,row] of [..._table.rows].entries()){
+}
+//add Column function
+function addColumn() {
+    for (const [i, row] of [..._table.rows].entries()) {
         const cell = document.createElement(i ? "td" : "th");
-        if(i == 0){
+        if (i == 0) {
             cell.innerText = "Heading";
             row.appendChild(cell);
-        }else{
+        } else {
             cell.innerText = "data";
             row.appendChild(cell);
         }
     }
+}
 
-})
